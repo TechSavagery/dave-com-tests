1b376925c25d8b042fbcc1302582d85424e256a3
ladell.erby <ladellerby@techsavagery.net>
2021-07-02 13:49:25 -0700

    updating test report settings

--- cypress/integration/job-posts-qa-engineer.spec.js
+++ cypress/integration/job-posts-qa-engineer.spec.js
@@ -8,18 +8,13 @@ describe("Job Posts Tests", () => {
     });
   
     it("Finds Senior QA Engineer Posts", () => {
-      cy.fixture("viewports").then((viewports) =>{
-          viewports.forEach(viewport => {
-            cy.log(`Running on ${viewport}`)
-            //Arrange 
-            cy.viewport('macbook-13')
-            cy.visit(Cypress.env("baseUrl"))
-            //Act 
-            cy.contains('Careers').click()
-            //Assert
-            cy.contains('Sr. QA Engineer')
-          });
-      })
+        //Arrange 
+        cy.viewport('macbook-13')
+        cy.visit(Cypress.env("baseUrl"))
+        //Act 
+        cy.contains('Careers').click()
+        //Assert
+        cy.contains('Sr. QA Engineer')
     });
   });
   
